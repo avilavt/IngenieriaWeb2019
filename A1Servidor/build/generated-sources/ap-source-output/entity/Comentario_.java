60ae1e4b7d63e60ba0a5439b8edaf7af1285cfdc
AlaeAk <36894933+AlaeAk@users.noreply.github.com>
2019-11-12 20:17:31 +0100

    Add files via upload
    Proyecto corregido, funcionando con la BD y el pool de conexion.

---
+++ A1Servidor/build/generated-sources/ap-source-output/entity/Comentario_.java
@@ -0,0 +1,19 @@
+package entity;
+
+import entity.Tablon;
+import entity.Usuario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-12T20:13:34")
+@StaticMetamodel(Comentario.class)
+public class Comentario_ { 
+
+    public static volatile SingularAttribute<Comentario, String> contenido;
+    public static volatile SingularAttribute<Comentario, Tablon> idTablon;
+    public static volatile CollectionAttribute<Comentario, Usuario> usuarioCollection;
+    public static volatile SingularAttribute<Comentario, Integer> idComentario;
+
+}
