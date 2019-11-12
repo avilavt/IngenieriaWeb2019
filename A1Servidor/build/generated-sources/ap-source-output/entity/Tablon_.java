65166a5fa2269f9a3166c2f9309dab9a98b76efb
ak <ak@172.16.138.103>
2019-11-12 20:27:12 +0100

    Actualizado proyecto.

--- A1Servidor/build/generated-sources/ap-source-output/entity/Tablon_.java
+++ A1Servidor/build/generated-sources/ap-source-output/entity/Tablon_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-12T20:13:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-12T20:26:54")
 @StaticMetamodel(Tablon.class)
 public class Tablon_ { 
 
