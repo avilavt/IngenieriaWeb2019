911ba198870b9c643b473a69e57593cd5eefb82d
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2019-11-15 09:19:11 +0100

    Inserción de dos nuevos usuarios en tabla de usuarios

--- sql/datos_derby.sql
+++ sql/datos_derby.sql
@@ -1,3 +1,8 @@
 INSERT INTO USUARIO (EMAIL, NOMBRE, ROL) VALUES 
 ('pruebaparaingweb@gmail.com', 'Prueba', 'Administrador'),
 ('avilavt@gmail.com', 'Cristian Ávila', 'Administrador');
+
+INSERT INTO USUARIO (EMAIL, NOMBRE, ROL) VALUES 
+('senan996@gmail.com', 'Senan', 'Administrador'),
+('alaeak.aa@gmail.com', 'Alaeak', 'Administrador');
+
