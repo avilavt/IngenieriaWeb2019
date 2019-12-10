2a9e622032068695e05b36251c5d4f665295faee
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2019-12-10 15:33:48 +0100

    Borrado de método no válido

--- python/OpenDataWebService.py
+++ python/OpenDataWebService.py
@@ -47,15 +47,5 @@ def find_by_ogc_fid(id):
     laneList = Lane()
     return laneList.find_by_ogc_fid(id)
 
-
-"""
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-"""
-
 if __name__ == '__main__':
     app.run(debug=True)
